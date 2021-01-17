572b96c3c6d7c57e0ac4c86c3940faeed6c92af9
gjgd <gjgd@protonmail.com>
2021-01-17 18:59:12 +0100

    feat: add disableIfForbidden function that triggers on every body click

--- extension/content_script.js
+++ extension/content_script.js
@@ -22,29 +22,37 @@
     '5 min',
   ];
 
-  const timeSelectorButtons = document.getElementsByClassName('time-selector-button')
-  const largeButtons = document.getElementsByClassName(
-    'ui_v5-button-component ui_v5-button-primary ui_v5-button-large ui_v5-button-full',
-  );
-  console.info('time selector found', timeSelectorButtons.length === 1);
-  console.info('play button found', largeButtons.length === 1);
-  const playButton = largeButtons && largeButtons[0];
-  const timeSelectorButton = timeSelectorButtons[0];
-
-  // https://stackoverflow.com/questions/7381293/how-to-intercept-innerhtml-changes-in-javascript
-  timeSelectorButton.addEventListener('DOMSubtreeModified', function () {
-    const timeControl = timeSelectorButton.innerText;
-    console.info('time control was modified to', timeControl);
-    console.info(
-      'time control is forbidden',
-      forbiddenTimeControls.includes(timeControl),
+  function disableIfForbidden () {
+    const timeSelectorButtons = document.getElementsByClassName('time-selector-button')
+    const largeButtons = document.getElementsByClassName(
+      'ui_v5-button-component ui_v5-button-primary ui_v5-button-large ui_v5-button-full',
     );
-    if (forbiddenTimeControls.includes(timeControl)) {
+    const playButton = largeButtons && largeButtons[0];
+    const timeSelectorButton = timeSelectorButtons[0];
+    if (!playButton) {
+      console.info('play button not found');
+      return;
+    }
+    if (!timeSelectorButton) {
+      console.info('time selector button not found');
+      return;
+    }
+    const timeControl = timeSelectorButton.innerText;
+    console.info('time control is', timeControl);
+    const forbidden = forbiddenTimeControls.includes(timeControl);
+    console.info('time control is forbidden', forbidden);
+    if (forbidden) {
       playButton.disabled = true;
       playButton.style.backgroundColor = 'grey';
     } else {
       playButton.disabled = false;
       playButton.style.backgroundColor = '';
     }
+  }
+
+  const body = document.getElementsByTagName('body')[0];
+  // https://stackoverflow.com/questions/7381293/how-to-intercept-innerhtml-changes-in-javascript
+  body.addEventListener('click', function () {
+    disableIfForbidden();
   });
 })();
